d443d423f60ade69abd13de777d1619b516de0b8
Shawn Huggins <hugginsjrshawn@gmail.com>
2019-01-12 19:14:35 -0600

    Add an Article Preview component

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ArticlePreview from './ArticlePreview'
 
 const ArticleList = (props) => {
   if (!props.articles) {
@@ -19,7 +20,9 @@ const ArticleList = (props) => {
     <div>
       {
         props.articles.map(article => {
-          return (<h2>{article.title}</h2>)
+          return (
+            <ArticlePreview article={article} key={article.slug} />
+          )
         })
       }
     </div>
