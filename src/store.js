76d2707f7ea7c301a64909a6cf6e0d90bb7022bd
Shawn Huggins <hugginsjrshawn@gmail.com>
2019-01-12 19:31:26 -0600

    Seperate the redux store into its own module

---
+++ src/store.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, createStore } from 'redux'
+import { promiseMiddleware } from './middleware'
+
+const defaultState = {
+  appName: 'article-client',
+  articles: null
+}
+const reducer = function (state = defaultState, action) {
+  switch (action.type) {
+    case 'HOME_PAGE_LOADED':
+      return { ...state, articles: action.payload.articles }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer, applyMiddleware(promiseMiddleware))
+
+export default store
